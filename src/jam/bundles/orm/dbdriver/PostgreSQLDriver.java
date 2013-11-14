c742e3e9aa8475ebee918ae2a405667c5ad63b7c
DefeNder93 <alexdefender93@gmail.com>
2013-11-15 00:33:59 +0300

    проблема с коннектом к базе (только в блоке try catch)

--- src/jam/bundles/orm/dbdriver/PostgreSQLDriver.java
+++ src/jam/bundles/orm/dbdriver/PostgreSQLDriver.java
@@ -1,5 +1,11 @@
 package jam.bundles.orm.dbdriver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * TODO: написать аннотацию к классу
  */
@@ -7,7 +13,24 @@ public class PostgreSQLDriver implements DBDriver {
 
     @Override
     public boolean Connect() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+
+        String url = "jdbc:postgresql://192.168.1.5/db_builder";
+        String user = "defender";
+        String password = "1234";
+        Connection con = DriverManager.getConnection(url, user, password);
+
+        try{    // а так работает, но всё только в пределах одного блока
+            url = "jdbc:postgresql://192.168.1.5/db_builder";
+            user = "defender";
+            password = "1234";
+            con = DriverManager.getConnection(url, user, password);
+        }
+        catch (SQLException ex) {
+
+        }
+
+
+    return false;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
